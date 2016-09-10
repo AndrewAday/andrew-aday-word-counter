2fd528b2b1e4df46c52728de8aebfc2030a91b94
Andrew Aday <andrew.aday@columbia.edu>
2016-09-10 16:09:52 -0400

    first commit

---
+++ spec/andrew-aday-word-count-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AndrewAdayWordCountView from '../lib/andrew-aday-word-count-view';
+
+describe('AndrewAdayWordCountView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
