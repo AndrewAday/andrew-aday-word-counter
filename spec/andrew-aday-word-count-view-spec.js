38695e8715e036aa3ae0e4b4dd3eab905e342353
Andrew Aday <andrew.aday@columbia.edu>
2016-09-10 16:41:07 -0400

    Tests

--- spec/andrew-aday-word-count-view-spec.js
+++ spec/andrew-aday-word-count-view-spec.js
@@ -4,6 +4,6 @@ import AndrewAdayWordCountView from '../lib/andrew-aday-word-count-view';
 
 describe('AndrewAdayWordCountView', () => {
   it('has one valid test', () => {
-    expect('life').toBe('easy');
+    expect('life').toBe('life');
   });
 });
